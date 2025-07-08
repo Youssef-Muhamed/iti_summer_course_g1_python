7f8ef8a0c879407afc0ef950706be0e57eb41551
youssef <eng.youssef2023@gmail.com>
2025-07-08 20:12:21 +0300

    [IMP]

---
+++ Day4/script.js
@@ -0,0 +1,86 @@
+// "use strict"; // Enforce strict mode for better error checking
+// console.log(firstName); // string
+// // let firstName = "Youssef"; // Declare a variable using var
+// let firstName = "Youssef"; // Declare a variable using var
+
+// function sayHello() {
+//   console.log("Hello, iti");
+// }
+//  sayHello(); // Call the function to see the output
+
+// function doSum(num1 = 5, num2 = 10) {
+//   var result = num1 + num2; // Use var to declare the result variable
+//   return result// Output the result to the console
+// }
+
+// function hello(){
+//     return "Hello, ITI!";
+// }
+
+// var res = doSum(10,20) // Call the function with arguments 20 and 30
+
+// console.log(res + 100); // Call the function with arguments 20 and 30
+
+// let num = 10;
+// function printNum() {
+//     let num = 20; // Declare a variable with the same name inside the function
+//     console.log(num); // Output the value of num to the console
+// }
+//  // Output the global variable num
+// printNum()
+// console.log(num);
+// var num = 15; // Declare a variable x with value 10
+// &&  --> and operator
+// ||  --> or operator
+// !   --> not operator
+// console.log(); // Output the value of x to the console
+
+// var num = 15;
+// var x = 'x'; // Declare a variable x with value 10
+// // x++ // Declare a variable x with value 1010
+
+// console.log(x >> 2); // Output the value of x to the console
+
+
+// Conditional Statements
+// let num = 10; // Declare a variable num with value 10
+// if (num >= 10) {
+//   console.log("num is greater than or equal 10");
+// } 
+// else if (num <= 10) {
+//   console.log("num is less than 10");
+// } else {
+//   console.log("num is equal to 10");
+// }
+// var num1 = 100; // Declare a variable num1 with value 10
+// num2 = "10"; // Declare a variable num2 with value 20
+// // refernceed value
+// if (num1 == 100) {
+//     var num3 = 200; // Declare a variable num3 with value 30
+//     console.log("yes");
+// }
+// var num = 10 +1 ; // Declare a variable num1 with value 10
+// console.log("First"); // Output the value of num1 to the console
+// console.log(num) // Output the value of num3 to the console
+// console.log("Second"); // Output the value of num3 to the console
+// switch (num1) {
+//   case 10:
+//     console.log("num1 is equal to 10");
+//     break;
+//   case 20:
+//     console.log("num1 is equal to 20");
+//     break;
+//   case 30:
+//     console.log("num1 is equal to 30");
+//     break;
+//   default:
+//     console.log("num1 is not equal to 10, 20, or 30");
+    
+// }
+// alert("Hello, ITI!"); // Display an alert with the message "Hello, ITI!"
+
+// var res = confirm("Are you sure you want to proceed?"); // Display a confirmation dialog with the message "Are you sure you want to proceed?"
+// console.log(res); // Output the result of the confirmation dialog to the console
+
+// var name = prompt("Please enter your name:"); // Display a prompt dialog asking the user to enter their name
+// console.log("Hello, " + name + "!"); // Output a greeting message with the user's
